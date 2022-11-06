7e826187d0bd0c0ea43e01cb37738c7b5d2be735
Aseem Yaseen <asemyassen300@gmail.com>
2022-11-06 22:54:47 +0300

    Tasks

---
+++ Sunday_6_11_2022/Task2.js
@@ -0,0 +1,42 @@
+
+
+
+// let a = " "
+// let b = 1;
+
+// for (let i = 0 ; i < 4 ; i++){
+ 
+//     for(y=0; y<=i ; y++) {
+//         a = a+b
+//         b+=1
+
+//     }
+//     a+= '\n';
+// }
+// console.log(a)
+
+
+for (let i = 0 ; i<4; i++) {
+
+    let x =i+1
+
+    if (x == 1 ) {
+        console.log (x)
+    }
+
+    else if (x == 2)
+
+console.log (x, x+1)
+
+else if (x == 3 )
+
+console.log(x=1+x ,1+x , 2+x )
+
+else if (x=4) {
+
+    console.log (x=3+x , 1+x ,2+x , 3+x)
+}
+
+
+}
+
---
+++ test.js
@@ -0,0 +1,100 @@
+// let num1 = Number(prompt("Enter X Value"));
+// let num2 = Number(prompt("Enter Y Value"));
+
+// Number(num1)
+// Number(num2)
+
+// if (num1 > num2) {
+//     console.log('X is bigger')
+// }
+// else if ( num1 < num2) {
+//     console.log('Y is bigger')
+// }
+
+// else {
+//     console.log('X and Y have the same value')
+// }
+
+
+
+
+// && and
+// || or
+// == equal
+// != not equal
+
+
+
+
+                    // Switch Example
+
+
+// let Fruits = prompt("Enter the fruit kind here to know the price")
+
+// switch (Fruits) {
+
+//     case "mango"  :console.log(`Mango price is 3.5$`); 
+//     break;
+    
+
+//     case "banana" : console.log (`Banana price is 1.8$`);
+//      break;
+    
+
+//     case "orange" : console.log (`Orange price is 2.2$`); 
+//     break;
+    
+
+//     case "apple" : console.log (`*Apple price is1.2$`); 
+//     break;
+
+//     default:console.log(`We dont have this kind`)
+
+
+// }
+
+
+
+// let x = [`Test1` , `Test2` , `Test3` , `Test4`];
+// let x2 = [ `a` , `b` , `c` , `d`]
+
+// for(let y = 0 ; y < x.length ; y++ ) {
+// console.log(`alltest ${x[y]}`)
+
+// for (let y2 =0; y2 <x2.length ; y2++ ) {
+// console.log (`this is y2 ${x2[y2]}`)
+// }
+
+
+// }
+
+
+
+// let names = [`Asem` ,  `Ahmad` , `Abood` , `Rami`]
+// let money = [4000 , 1200 , 800 , 3400]
+
+// for (let b = 0 ; b < names.length ; b++) {
+//     console.log(`These are names ${names[b]}`)
+
+//     for (let c =0 ; c < money.length ; c++)
+//     console.log(`This is money ${money[c]}`)
+// }
+
+
+
+// console.log("Starting Loop" + "<br />");
+//             // count = 0
+//             sum = 0
+//             for(var count = 0; count <= 100; count++) {
+//                sum = sum + count
+//                if (sum > 2000) {
+//                 break;
+//                }
+//             // count++
+//             }
+//             console.log("Loop stopped!");
+//             console.log("stoped number is = " +count);
+//             console.log("the sum = " +sum);
+
+
+
