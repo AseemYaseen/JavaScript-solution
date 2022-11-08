cb3d5e82b07b383cba131a2ff5a51b083722eb8f
Aseem Yaseen <asemyassen300@gmail.com>
2022-11-08 23:14:10 +0300

    Tasks

--- Tuesday_8_11_2022/Task10.js
+++ Tuesday_8_11_2022/Task10.js
@@ -1,8 +1,19 @@
-function indexOfArray(array , index) {
 
-      return array [index]
+
+function indexOfArray(array1 , index1) {
+
+
+
+      return array1 [index1]
+
     }
-    console.log(indexOfArray(`Test`, [1,2,3,8,9]));
+
+    var nums= [1,2,3,8,9]
+    console.log(indexOfArray(nums,0))
+    console.log(indexOfArray(nums,1))
+    console.log(indexOfArray(nums,2))
+    console.log(indexOfArray(nums,3)) 
+    console.log(indexOfArray(nums,4))
 
 
 
