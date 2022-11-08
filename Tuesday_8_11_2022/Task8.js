789bde52211c334ef1149f6244b3db3d935f9d84
Aseem Yaseen <asemyassen300@gmail.com>
2022-11-09 01:34:15 +0300

    Tasks

--- Tuesday_8_11_2022/Task8.js
+++ Tuesday_8_11_2022/Task8.js
@@ -1,13 +1,19 @@
-function middleOfArray (array1) {
 
-     if ( array1.length % 2 === 0){
-        
-      console.log(array1.splice((array1.length-1)/2 , array1.length/2 ))
-      }
-      else {
-         console.log(array1[((array1.length-1)/2)])
-     }
-    }
-middleOfArray([1,4,5])
-middleOfArray(["t","u","g","x"])
-    
+function middleOfArray (array1) {
+   
+   if(array1.length %2 == 0){
+
+      return array1.splice(Math.floor((array1.length-1)/2) , array1.length/2)
+
+   }
+
+   else {
+      return array1[(array1.length-1)/2]
+             
+   }
+
+
+}
+
+console.log(middleOfArray(["t","u","g","x"]))
+console.log(middleOfArray([1,4,5]))
